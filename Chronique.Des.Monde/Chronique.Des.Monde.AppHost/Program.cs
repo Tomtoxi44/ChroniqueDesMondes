e7e7981e4f6dd9c13bf036833a4d86364c88060a
A.Tommy <tommy.angibaud@gmail.com>
2025-06-02 21:28:13 +0200

    Init aspire

---
+++ Chronique.Des.Monde/Chronique.Des.Monde.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.Chronique_Des_Monde_ApiService>("apiservice");
+
+builder.AddProject<Projects.Chronique_Des_Monde_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
