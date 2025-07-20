7cc7ab63cb137da571d4b01bded503344a250566
A.Tommy <tommy.angibaud@gmail.com>
2025-07-20 12:40:35 +0200

    Configures authentication and user management

--- Chronique.Des.Monde/Chronique.Des.Mondes.ApiService/Program.cs
+++ Chronique.Des.Monde/Chronique.Des.Mondes.ApiService/Program.cs
@@ -1,6 +1,9 @@
+using Chronique.Des.Mondes.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Chronique.Des.Mondes.ApiService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,7 @@ builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
+builder.Services.AddApplicationServices();
 
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -26,10 +30,13 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-builder.Services.AddScoped<JwtService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -44,11 +51,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.MapWeatherEndpoints();
+app.MapApplicationEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
