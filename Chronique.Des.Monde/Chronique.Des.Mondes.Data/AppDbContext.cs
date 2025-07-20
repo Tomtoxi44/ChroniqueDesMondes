42c5251e54a102a145e8c39d6c274d77c8f728cc
A.Tommy <tommy.angibaud@gmail.com>
2025-07-20 14:03:57 +0200

    [US #13] #16 ⚠️Adds PlayerCharacters table and model

--- Chronique.Des.Monde/Chronique.Des.Mondes.Data/AppDbContext.cs
+++ Chronique.Des.Monde/Chronique.Des.Mondes.Data/AppDbContext.cs
@@ -7,4 +7,5 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Users> Users { get; set; }
+    public DbSet<PlayerCharacter> PlayerCharacter { get; set; }
 }
