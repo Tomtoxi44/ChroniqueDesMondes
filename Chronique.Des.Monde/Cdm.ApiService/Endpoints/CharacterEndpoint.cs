9203f3a14f3c01e86ee967d067da9f13a6aee96a
A.Tommy <tommy.angibaud@gmail.com>
2025-08-17 21:07:59 +0200

    Un truc

--- Chronique.Des.Monde/Cdm.ApiService/Endpoints/CharacterEndpoint.cs
+++ Chronique.Des.Monde/Cdm.ApiService/Endpoints/CharacterEndpoint.cs
@@ -13,24 +13,24 @@ public static class CharacterEndpoint
     {
         var characterCollectionGroup = app.MapGroup("/character").RequireAuthorization();
 
-        app.MapGet(string.Empty, async (int userId, HttpRequest httpRequest,[FromHeader(Name = "X-GameType")] string gameType, IServiceProvider serviceProvider) =>
-        {
-            var serviceCharacter = serviceProvider.GetRequiredKeyedService<ICharacterBusiness>(gameType);
-            serviceCharacter.GetCharacterByCharacterId(userId);
+        // app.MapGet(string.Empty, async (int userId, HttpRequest httpRequest,[FromHeader(Name = "X-GameType")] string gameType, IServiceProvider serviceProvider) =>
+        // {
+        //     var serviceCharacter = serviceProvider.GetRequiredKeyedService<ICharacterBusiness>(gameType);
+        //     serviceCharacter.GetCharacterByCharacterId(userId);
+        //
+        //     try
+        //     {
+        //         // var result = serviceCharacter.GetAllCharacterDnd(userId);
+        //         //return Results.Ok(result);
+        //         return Results.Ok();
+        //     }
+        //     catch (BusinessException ex)
+        //     {
+        //         return Results.BadRequest(new { Error = ex.Message });
+        //     }
+        // });
 
-            try
-            {
-                // var result = serviceCharacter.GetAllCharacterDnd(userId);
-                //return Results.Ok(result);
-                return Results.Ok();
-            }
-            catch (BusinessException ex)
-            {
-                return Results.BadRequest(new { Error = ex.Message });
-            }
-        });
-
-        app.MapGet($"/characterId", async(int characterId, HttpRequest httpRequest, [FromHeader(Name = "X-GameType")] string gameType, IServiceProvider serviceProvider) =>
+        app.MapGet($"/characterId", (int characterId, HttpRequest httpRequest, [FromHeader(Name = "X-GameType")] string gameType, IServiceProvider serviceProvider) =>
         {
             var serviceCharacter = serviceProvider.GetRequiredKeyedService<ICharacterBusiness>(gameType);
             serviceCharacter.GetCharacterByCharacterId(characterId);
@@ -46,41 +46,41 @@ public static class CharacterEndpoint
             }
         });
 
-        app.MapPost(string.Empty, async (CharacterDndRequest dndRequest, int userId, HttpRequest httpRequest, [FromServices] CharacterDndBusiness dndBusiness) =>
-            {
-                var token = httpRequest.Headers["Authorization"].ToString().Replace("Bearer ", "");
-
-                if (string.IsNullOrEmpty(token))
-                    return Results.BadRequest(new { Error = "Le token est manquant." });
-
-                try
-                {
-                    await dndBusiness.CreateCharacterDndRequestAsync(dndRequest, userId);
-                    return Results.Created("/player-character", null);
-                }
-                catch (BusinessException ex)
-                {
-                    return Results.BadRequest(new { Error = ex.Message });
-                }
-        });
-
-        app.MapPut($"playerId", async (CharacterDndRequest dndRequest, int playerId, HttpRequest httpRequest, [FromServices] CharacterDndBusiness dndBusiness) =>
-        {
-            var token = httpRequest.Headers["Authorization"].ToString().Replace("Bearer ", "");
-
-            if (string.IsNullOrEmpty(token))
-                return Results.BadRequest(new { Error = "Le token est manquant." });
-
-            try
-            {
-                var result = dndBusiness.UpdateCharacterDndAsync(dndRequest, playerId);
-                return Results.Ok();
-            }
-            catch (BusinessException ex)
-            {
-                return Results.BadRequest(new { Error = ex.Message });
-            }
-        });
+        // app.MapPost(string.Empty, async (CharacterDndRequest dndRequest, int userId, HttpRequest httpRequest, [FromServices] CharacterDndBusiness dndBusiness) =>
+        //     {
+        //         var token = httpRequest.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        //
+        //         if (string.IsNullOrEmpty(token))
+        //             return Results.BadRequest(new { Error = "Le token est manquant." });
+        //
+        //         try
+        //         {
+        //             await dndBusiness.CreateCharacterDndRequestAsync(dndRequest, userId);
+        //             return Results.Created("/player-character", null);
+        //         }
+        //         catch (BusinessException ex)
+        //         {
+        //             return Results.BadRequest(new { Error = ex.Message });
+        //         }
+        //     });
+        //
+        // app.MapPut($"playerId", async (CharacterDndRequest dndRequest, int playerId, HttpRequest httpRequest, [FromServices] CharacterDndBusiness dndBusiness) =>
+        // {
+        //     var token = httpRequest.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        //
+        //     if (string.IsNullOrEmpty(token))
+        //         return Results.BadRequest(new { Error = "Le token est manquant." });
+        //
+        //     try
+        //     {
+        //         var result = dndBusiness.UpdateCharacterDndAsync(dndRequest, playerId);
+        //         return Results.Ok();
+        //     }
+        //     catch (BusinessException ex)
+        //     {
+        //         return Results.BadRequest(new { Error = ex.Message });
+        //     }
+        // });
     }
     //private IResult GetAllCharacter()
     //{
